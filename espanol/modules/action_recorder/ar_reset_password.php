c2780d01d12835335ee858d664d45c054350e565
aldortech <118578472+aldortech@users.noreply.github.com>
2022-11-18 20:11:25 -0500

    Add files via upload

---
+++ espanol/modules/action_recorder/ar_reset_password.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2017 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ACTION_RECORDER_RESET_PASSWORD_TITLE', 'Restablecer contraseña del cliente');
+  define('MODULE_ACTION_RECORDER_RESET_PASSWORD_DESCRIPTION', 'Registro de uso del cliente del restablecimiento de contraseñas.');
+  
